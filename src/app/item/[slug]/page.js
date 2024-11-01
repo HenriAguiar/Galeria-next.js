bc66e0d4e5cccb43852c35cac8e710fee72dac7d
HenriAguiar <aguiar.henri1@gmail.com>
2024-11-01 16:41:49 -0300

    Feat: Cria arquivo page.js da pagina de detalhe do item

---
+++ src/app/item/[slug]/page.js
@@ -0,0 +1,14 @@
+export default function Item() {
+  return (
+    <>
+      <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-white">
+        <h1 className="text-7xl font-black text-[#efe1ce] drop-shadow-2xl">
+          POKEDEX
+        </h1>
+        <p className="font-semibold text-4xl mt-4 text-[#efe1ce] drop-shadow-2xl">
+          Seu portal para monstros japoneses
+        </p>
+      </div>
+    </>
+  );
+}
