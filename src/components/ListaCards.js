9f801135a7452aeef5a392eff210fd1b5094ddd6
HenriAguiar <aguiar.henri1@gmail.com>
2024-11-01 15:54:58 -0300

    Feat: Adiciona titulo e mensagem condicional na seção

--- src/components/ListaCards.js
+++ src/components/ListaCards.js
@@ -1,13 +1,32 @@
 import CardItem from "./CardItem";
 import { getAllItems } from "@/libs/services/item-service";
-
+import { Button, Card } from "flowbite-react";
 export default async function ListaCards() {
     const items = await getAllItems();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {items.map((item) => (
-        <CardItem key={item.id} item={item} />
-      ))}
+<div className=" bg-[#1E1E1E]">
+      <div className=" container flex-col mx-auto flex justify-center align-middle items-center gap-10 p-10">
+        <Card className="h-[4rem] border-none bg-[#7E2429] text-2xl font-bold text-[#EFE1CE] drop-shadow-2xl xs:w-4/5 md:w-2/5">
+          <div className="flex justify-center align-middle items-center xs:text-center md:text-justify">
+            GALERIA
+          </div>
+        </Card>
+
+        {items.length === 0 ? (
+          <p className=" text-2xl font-extrabold tracking-tight text-[#292929] dark:text-white">
+            Nenhum item foi encontrado
+          </p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
+            {items.map((item) => (
+              <CardItem
+                key={item.id}
+                item={item}
+              ></CardItem>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
