3dddcd2e452db3b3b4157352da4eeb4e4e100c2b
HenriAguiar <aguiar.henri1@gmail.com>
2024-11-01 16:40:49 -0300

    Feat: Adiciona valores padrão no modelo do bd

---
+++ prisma/migrations/20241101191100_define_valores_default/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Item" ALTER COLUMN "titulo" SET DEFAULT 'Charmander',
+ALTER COLUMN "descricao" SET DEFAULT 'Charmander é um Pokémon do tipo Fogo conhecido por sua aparência fofa e amigável, além de sua cauda flamejante característica, que reflete seu estado de saúde e emoções. Quando nascido, ele é pequeno, com pele laranja e olhos grandes e expressivos. A chama na ponta de sua cauda nunca se apaga e aumenta de intensidade conforme ele se emociona ou se enfurece, sendo um indicativo de sua vitalidade.',
+ALTER COLUMN "categoria" SET DEFAULT 'Fogo',
+ALTER COLUMN "imagemCard" SET DEFAULT 'https://mir-s3-cdn-cf.behance.net/projects/404/88c776175433921.Y3JvcCwxMDMxLDgwNiw0NDksMjE0.png',
+ALTER COLUMN "imagemCapa" SET DEFAULT 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/a7a4ba175433921.64b3dc10c047e.png';
