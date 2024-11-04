d17a75b7107f35402e71d9cfb049d12d8fb6c118
Henrique <aguiar.henri1@gmail.com>
2024-11-03 22:15:39 -0300

    Fix: Corrige tamanho do texto em telas sm

--- src/components/CapaItem.js
+++ src/components/CapaItem.js
@@ -20,10 +20,10 @@ export default function CapaItem({ titulo, descricao, categoria, imagem }) {
             <div className="bg-gradient-to-r from-[rgba(30,30,30,0.11)] to-[rgba(30,30,30,0.73)]  absolute inset-0 " style={{
                 background: 'linear-gradient(270deg, rgba(30, 30, 30, 0.11) 43.5%, rgba(30, 30, 30, 0.73) 95%)'
             }}></div>
-            <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-white grid grid-cols-1 md:grid-cols-2 ">
+            <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-white grid grid-cols-1 lg:grid-cols-2 ">
                 <div className="flex flex-col items-start gap-8 p-10">
-                    <h1 className="text-start truncate-titulo text-8xl font-black text-[#efe1ce] drop-shadow-2xl">{titulo}</h1>
-                    <p className="text-start truncate-descricao font-semibold text-3xl text-[#efe1ce] drop-shadow-2xl">{descricao}</p>
+                    <h1 className="text-start truncate-titulo font-black text-[#efe1ce] drop-shadow-2xl break-words text-4xl md:text-6xl lg:text-7xl xl:text-8xl max-w-[100%]">{titulo}</h1>
+                    <p className="text-start truncate-descricao font-semibold mt-4 text-[#efe1ce] drop-shadow-2xl break-words text-lg md:text-2xl lg:text-3xl xl:text-4xl max-w-[100%]">{descricao}</p>
                     <Badge
                         className="px-3 rounded-lg"
                         theme={badgeTheme}
