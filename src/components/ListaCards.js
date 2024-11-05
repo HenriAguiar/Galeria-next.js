7c2772be6e12f6186175f51cbfa6883c764764a9
HenriAguiar <aguiar.henri1@gmail.com>
2024-11-05 11:20:35 -0300

    Fix: Corrige responsividade do card do titulo da seção

--- src/components/ListaCards.js
+++ src/components/ListaCards.js
@@ -6,8 +6,8 @@ export default async function ListaCards() {
   return (
 <div className=" bg-[#1E1E1E]">
       <div className=" container flex-col mx-auto flex justify-center align-middle items-center gap-10 p-10">
-        <Card className="h-[4rem] w-[25vw] max-w-[40rem] border-none bg-[#7E2429] text-2xl font-bold text-[#EFE1CE] drop-shadow-2xl">
-          <div className="p-[80px] flex justify-center align-middle items-center xs:text-center md:text-justify">
+        <Card className="h-[4rem] mx-5 border-none bg-[#7E2429] text-2xl font-bold text-[#EFE1CE] drop-shadow-2xl">
+          <div className="p-[80px] flex justify-center align-middle items-center">
             GALERIA
           </div>
         </Card>
